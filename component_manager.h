a46d90f8c13ea36dd83402649138ac8c2830e271
Zotov V <tomasloh@yandex.ru>
2019-04-10 22:29:03 +0300

    Add ability to set component for a entity

--- component_manager.h
+++ component_manager.h
@@ -6,6 +6,10 @@
 #ifndef COMPONENT_MANAGER_H
 #define COMPONENT_MANAGER_H
 
+#include "entity_manager.h"
+#include <map>
+#include <utility>
+
 namespace esc
 {
 
@@ -14,6 +18,31 @@ namespace esc
  */
 class ComponentManager
 {
+
+  public:
+    /**
+     * @brief AddComponent to entity
+     * 
+     * @tparam T Type of component
+     * @tparam Args Component arguments types
+     * @param entity Entity to add component to
+     * @param args Values for component arguments
+     */
+    template <typename T, typename... Args>
+    void add(const esc::EntityManager::EntityId entity, Args &&... args)
+    {
+        T* component = new T({std::forward<Args>(args)...});
+        _components.insert(std::make_pair(entity, static_cast<void*>(component)));
+    }
+
+  private:
+    /**
+     * @brief Store entities and components
+     * 
+     * Key is entity
+     * Value is pointer to component
+     */
+    std::multimap<esc::EntityManager::EntityId, void *> _components;
 };
 
 } // namespace esc
--- test/test_component_manager.cpp
+++ test/test_component_manager.cpp
@@ -2,10 +2,30 @@
 #include <boost/test/included/unit_test.hpp>
 
 #include <component_manager.h>
+#include <entity_manager.h>
 
-BOOST_AUTO_TEST_CASE(constructor)
+BOOST_AUTO_TEST_CASE(componentManager_constructor)
 {
     esc::ComponentManager manager;
+
+    BOOST_TEST(true);
+}
+
+struct Position
+{
+    int x;
+    int y;
+};
+
+BOOST_AUTO_TEST_CASE(component)
+{
+
+    esc::ComponentManager components;
+    esc::EntityManager entities;
+
+    esc::EntityManager::EntityId entity = entities.create();
+
+    components.add<Position>(entity, 10, 15);
 
     BOOST_TEST(true);
 }
