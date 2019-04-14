50a8c8203dc44e7909760462c9fd90d97142fe59
Zotov V <tomasloh@yandex.ru>
2019-04-14 21:14:02 +0300

    Add couple of tests for Gravity system init

--- test/test_gravity_system.cpp
+++ test/test_gravity_system.cpp
@@ -53,3 +53,39 @@ BOOST_AUTO_TEST_CASE(gravity_system_init)
 
     BOOST_TEST(system.init());
 }
+
+BOOST_AUTO_TEST_CASE(gravity_system_init_should_fail_if_no_position_manager)
+{
+    esc::Systems::Gravity system;
+    esc::Components::ForceManager f_manager;
+    esc::EntityManager e_manager;
+
+    system.set(&f_manager);
+    system.set(&e_manager);
+
+    BOOST_TEST(!system.init());
+}
+
+BOOST_AUTO_TEST_CASE(gravity_system_init_should_fail_if_no_force_manager)
+{
+    esc::Systems::Gravity system;
+    esc::Components::PositionManager p_manager;
+    esc::EntityManager e_manager;
+
+    system.set(&p_manager);
+    system.set(&e_manager);
+
+    BOOST_TEST(!system.init());
+}
+
+BOOST_AUTO_TEST_CASE(gravity_system_init_should_fail_if_no_entity_manager)
+{
+    esc::Systems::Gravity system;
+    esc::Components::PositionManager p_manager;
+    esc::Components::ForceManager f_manager;
+
+    system.set(&p_manager);
+    system.set(&f_manager);
+
+    BOOST_TEST(!system.init());
+}
